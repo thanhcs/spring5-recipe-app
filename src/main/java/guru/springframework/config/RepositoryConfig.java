941560de36a123eab0081d9bd1ba144058d64472
Thanh Nguyen <thanhnguyencs@outlook.com>
2020-08-20 00:04:38 -0500

    Optimizing component scan
    Introducing config with RepositoryConfig

---
+++ src/main/java/guru/springframework/config/RepositoryConfig.java
@@ -0,0 +1,9 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "guru.springframework.repository")
+public class RepositoryConfig {
+}
